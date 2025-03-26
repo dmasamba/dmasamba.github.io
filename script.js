1e783b67ac453de08c29c8431918945307bc3866
Daniel Masamba <103907426+dmasamba@users.noreply.github.com>
2025-03-25 20:45:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".sidebar nav ul li a");
+    const toggleButton = document.getElementById("toggle-mode");
+    toggleButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+
+    const links = document.querySelectorAll(".navbar ul li a");
     links.forEach(link => {
         link.addEventListener("click", function (e) {
             e.preventDefault();
             const targetId = this.getAttribute("href").substring(1);
             const targetElement = document.getElementById(targetId);
             window.scrollTo({
-                top: targetElement.offsetTop - 20,
+                top: targetElement.offsetTop - 50,
                 behavior: "smooth"
             });
         });
